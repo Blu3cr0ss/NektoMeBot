d56346c0421c7a4cf811b47df1c968380a60747f
hedgegod <werstak192@gmail.com>
2023-01-09 19:59:33 +0300

    Minor changes added to DiscordBot.kt

--- src/main/java/bot/nektome/nektobot/DiscordBot.kt
+++ src/main/java/bot/nektome/nektobot/DiscordBot.kt
@@ -4,18 +4,20 @@ import bot.nektome.nektobot.util.logger
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 import kotlin.system.exitProcess
 
 class DiscordBot: ListenerAdapter() {
-    public fun main(args: Array<String>){
+    fun main(args: Array<String>){
         if (args.isEmpty()) {
             logger.info("You have to provide a token as first argument!");
             exitProcess(1);
         }
 
         val jda = JDABuilder.createLight(args[0], emptyList()).addEventListeners(DiscordBot()).setActivity(Activity.playing("NektoMe")).build()
+        jda.awaitReady()
         jda.updateCommands().addCommands(Commands.slash("ping", "Calculate ping of the bot"))
     }
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent){
@@ -25,5 +27,11 @@ class DiscordBot: ListenerAdapter() {
                 .flatMap { _ -> event.hook.editOriginalFormat("Pong: %d ms", System.currentTimeMillis() - time)}
                 .queue()
         }
+    }
+
+    override fun onMessageReceived(event: MessageReceivedEvent) {
+        var author = event.author
+        var message = event.message
+        System.out.println("$author sent $message")
     }
 }
